c69ca6ac7227f6bf7a1ee319dd69eafc2c67d07e
tfmagician <tfmagician@gmail.com>
2011-01-01 08:56:07 +0900

    Developed CounterComponent class.

---
+++ controllers/components/counter.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Counter component.
+ *
+ * @package hits
+ * @subpackage hits.controllers.components
+ */
+class CounterComponent extends Object
+{
+
+    /**
+     * Environments ignored.
+     *
+     * @access public
+     * @var array  Could have 'ipAddresses' and 'userAgents' keys.
+     */
+    var $ignore = array(
+        'ipAddresses' => array(),
+        'userAgents' => array(),
+    );
+
+    /**
+     * Components to use.
+     *
+     * @access public
+     * @var array
+     */
+    var $components = array(
+        'RequestHandler',
+    );
+
+    /**
+     * Initialization
+     *
+     * @access public
+     * @param object $Controller  Controller with components to initialize
+     * @param array $config
+     * @return void
+     */
+    function initialize(&$Controller, $config = array())
+    {
+        $this->Hit =& ClassRegistry::init('Hit');
+        $this->ignore = $config['ignore'] + $this->ignore;
+    }
+
+    /**
+     * Startup
+     *
+     * @access public
+     * @param object  $controller Controller with components to statup
+     * @return void
+     */
+    function startup(&$Controller)
+    {
+        if ($ip = $this->RequestHandler->getClientIP()) {
+            if (in_array($ip, $this->ignore['ipAddresses'])) {
+                return;
+            }
+        }
+        if ($agent = env('HTTP_UESR_AGENT')) {
+            foreach ($this->ignore['userAgents'] as $ignore) {
+                if (preg_match('/'.$ignore.'/iu', $agent)) {
+                    return;
+                }
+            }
+        }
+        $this->Hit->count($Controller->params['url']['url']);
+    }
+
+}
---
+++ tests/cases/controllers/components/counter.test.php
@@ -0,0 +1,105 @@
+<?php
+App::import('Component', 'Hits.Counter');
+App::import('Component', 'RequestHandler');
+App::import('Model', 'Hits.Hit');
+
+Mock::generate('Hit', 'CounterComponent_Hit');
+Mock::generate('RequestHandlerComponent', 'CounterComponent_RequestHandler');
+Mock::generatePartial(
+    'Controller', 'TestController',
+    array('redirect', 'cakeError')
+);
+
+class CounterComponentTestCase extends CakeTestCase
+{
+
+    var $fixtures = array(
+        'plugin.hits.hit',
+    );
+
+    function startTest()
+    {
+        $this->Controller = new TestController();
+        $this->CounterComponent = new CounterComponent();
+        $this->CounterComponent->Hit = new CounterComponent_Hit();
+        $this->CounterComponent->RequestHandler = new CounterComponent_RequestHandler();
+    }
+
+    function endTest()
+    {
+        unset($this->Controller);
+        unset($this->CounterComponent);
+    }
+
+    function testStartup()
+    {
+        $Hit = $this->CounterComponent->Hit;
+        $Hit->expectOnce('count', array('/this/is/url'));
+        $this->Controller->params = array(
+            'url' => array(
+                'url' => '/this/is/url',
+            ),
+        );
+        $this->CounterComponent->startup($this->Controller);
+    }
+
+    function testStartupIgnoredByIPAddress()
+    {
+        $Hit = $this->CounterComponent->Hit;
+        $Hit->expectOnce('count', array('/this/is/url'));
+
+        $RequestHandler = $this->CounterComponent->RequestHandler;
+        $RequestHandler->expectCallCount('getClientIP', 3);
+        $RequestHandler->expectAt(0, 'getClientIP', array());
+        $RequestHandler->setReturnValueAt(0, 'getClientIP', '100.100.100.102');
+        $RequestHandler->expectAt(1, 'getClientIP', array());
+        $RequestHandler->setReturnValueAt(1, 'getClientIP', '100.100.100.100');
+        $RequestHandler->expectAt(2, 'getClientIP', array());
+        $RequestHandler->setReturnValueAt(2, 'getClientIP', '100.100.100.101');
+
+        $this->Controller->params = array(
+            'url' => array(
+                'url' => '/this/is/url',
+            ),
+        );
+        $this->CounterComponent->ignore = array(
+            'ipAddresses' => array(
+                '100.100.100.100',
+                '100.100.100.101',
+            ),
+        );
+        for ($i = 0; $i < 3; $i ++) {
+            $this->CounterComponent->startup($this->Controller);
+        }
+    }
+
+    function testStartupIgnoredUserAgent()
+    {
+        $Hit = $this->CounterComponent->Hit;
+        $Hit->expectOnce('count', array('/this/is/url'));
+
+        $this->Controller->params = array(
+            'url' => array(
+                'url' => '/this/is/url',
+            ),
+        );
+        $this->CounterComponent->ignore = array(
+            'userAgents' => array(
+                'ignore1',
+                'IGNORE2',
+            ),
+        );
+        $agents = array(
+            'ignore1',
+            'ignore2',
+            'agent',
+        );
+        foreach ($agents as $agent) {
+            $_SERVER['HTTP_UESR_AGENT'] = $agent;
+            $this->CounterComponent->startup($this->Controller);
+        }
+    }
+
+
+
+}
